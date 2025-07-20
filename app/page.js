2599d737bd4d79b106e13c1a57180c12341e9218
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-20 19:18:15 +0300

    L-515. Add ErrorBoundary that handles errors and wrap the other Home page components with it.

--- app/page.js
+++ app/page.js
@@ -3,21 +3,31 @@ import fs from 'node:fs/promises';
 import { Suspense } from 'react';
 
 import UsePromiseDemo from '@/components/UsePromisesDemo';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default async function Home() {
-  const fetchUsersPromise = new Promise((resolve) =>
+  const fetchUsersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile('dummy-db.json', 'utf-8');
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error('Error!'));
     }, 2000)
   );
 
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      {/**
+       * Optimal components structure (from inner to outer):
+       * - Component that handles data fetching (UsePromiseDemo)
+       * - Component that handles the loading state (Suspense)
+       * - Component that handles the errors (ErrorBoundary)
+       */}
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
