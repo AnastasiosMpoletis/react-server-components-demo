f2540e98c8aa4f8cd23b6f06946dca6ec46986a3
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-20 18:01:42 +0300

    L-508. Add RSCDemo and ClientDemo components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import ClientDemo from "@/components/ClientDemo";
+import RSCDemo from "@/components/RSCDemo.js";
+
 export default function Home() {
   return (
     <main>
-      <p>Let's go!</p>
+      <RSCDemo />
+      <ClientDemo />
     </main>
   );
 }
